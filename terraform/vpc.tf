2376fa9363901eb26abf3dd9e40034e4f8a376b6
Louis Nguyen <47875922+louisnguyen88@users.noreply.github.com>
2024-12-01 11:49:47 +1100

    Add Kuztomization

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
 
   name = "eks-vpc"
@@ -9,9 +9,9 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_dns_hostnames   = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
   tags = {
     Terraform   = "true"
@@ -26,6 +26,6 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/role/elb"                   = "1"
   }
 }
